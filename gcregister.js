2944fc0d460dc06683127ddab7a7f6346da89879
Swarnya Srivastava <49378967+Swarnya246@users.noreply.github.com>
2020-08-06 18:36:18 -0700

    Update gcregister.js

--- gcregister.js
+++ gcregister.js
@@ -8,12 +8,15 @@ firebase.auth().onAuthStateChanged(function(user) {
 
 		document.getElementById("user_div").style.display = "block";
 		document.getElementById("topnav-right").style.display = "inline-flex";
+		document.getElementById("auth").style.display = "none";
 		showMyGiftCards()
 
 	} else {
 		// No user is signed in.
-
-		window.location.replace("http://mporio.github.io/")
+		
+		document.getElementById("user_div").style.display = "none";
+		document.getElementById("topnav-right").style.display = "none";
+		document.getElementById("auth").style.display = "flex";
 	}
 });
 
